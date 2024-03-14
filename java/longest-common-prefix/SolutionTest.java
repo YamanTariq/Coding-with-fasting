32336c75e092aea8aa92d2112f1f46f3b15ef5bd
YeaMan0087 <yamantariq1@gmail.com>
2024-03-14 05:06:01 +0500

    added the test class

---
+++ java/longest-common-prefix/SolutionTest.java
@@ -0,0 +1,14 @@
+public class SolutionTest{
+
+	public static void main(String args[]){
+		
+		Solution test = new Solution();
+		
+		String[] arr = {"s", "sa", ""};
+		
+		System.out.println(test.longestCommonPrefix(arr));
+	
+		
+	}
+
+}
